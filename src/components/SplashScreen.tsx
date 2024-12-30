cb2b3e2dceaa20d753205458fc16aaffb1b13908
Sujay <sujay@equihome.com.au>
2024-12-30 16:36:49 +0800

    Trigger deployment with password auth

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,4 +1,4 @@
-// Trigger deployment
+// Trigger new deployment with password auth
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
