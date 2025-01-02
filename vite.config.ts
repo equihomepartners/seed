471c3581eafbb74b1c1089351466b853fcdfa912
Sujay <sujay@equihome.com.au>
2025-01-02 12:02:20 +0800

    Add CNAME for custom domain and update base URL

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/equihome-dashboard/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
